a53e6b1e519f599df71f05e1af64ea401d5e95d0
William Rodrigues <williamrodrigues2442@gmail.com>
2022-11-01 20:16:30 -0300

    feat: creates inventory CRUD

--- backend/inventory/main.py
+++ backend/inventory/main.py
@@ -1,22 +1,95 @@
 import os
-from typing import Union
+from typing import Any
 
-from redis_om import get_redis_connection
-from fastapi import FastAPI
+from redis_om import get_redis_connection, HashModel
+from fastapi import FastAPI, HTTPException, status, Response
+from fastapi.middleware.cors import CORSMiddleware
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = FastAPI()
-redis_conn = get_redis_connection()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+redis = get_redis_connection(
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT")),
+    password=os.getenv("REDIS_PASSWORD"),
+    decode_responses=True,
+)
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+class Product(HashModel):
+    name: str
+    price: float
+    quantity_available: int
+
+    class Meta:
+        database = redis
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def format_output(pk: str) -> dict[str, Any]:
+    product = Product.get(pk)
+    return {
+        "id": product.pk,
+        "name": product.name,
+        "price": product.price,
+        "quantity_available": product.quantity_available,
+    }
+
+
+@app.get(
+    "/api/products",
+    summary="Get all products",
+    tags=["Products"],
+)
+async def get_products():
+    return [format_output(pk) for pk in Product.all_pks()]
+
+
+@app.get(
+    "/api/products/{product_pk}",
+    summary="Get a product based on his pk",
+    tags=["Products"],
+)
+async def get_product(pk: str):
+    try:
+        return Product.get(pk)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Product with pk {pk} was not found",
+        )
+
+
+@app.post(
+    "/api/products",
+    summary="Creates a new product",
+    tags=["Products"],
+)
+async def create_product(product: Product):
+    return product.save()
+
+
+@app.delete(
+    "/api/products/{product_pk}",
+    summary="Delete a product based on his pk",
+    tags=["Products"],
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_class=Response,
+)
+async def delete_product(pk: str):
+    deleted = Product.delete(pk)
+    
+    if not deleted:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Product with pk {pk} was not found",
+        )
